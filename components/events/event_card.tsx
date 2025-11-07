7cdb0558da06574c640cfc9f1ca40768dc5f61f8
juan <juanjuangmo@gmail.com>
2025-11-07 16:31:48 +0100

    Refactored component folder structure

---
+++ components/events/event_card.tsx
@@ -0,0 +1,26 @@
+// components/EventCard.tsx
+import { View, Text } from "react-native";
+import { Event } from "@/models/Event";
+import { StyleSheet } from "react-native";
+
+export default function EventCard({event}: { event: Event }) {
+  return (
+    <View style={styles.eventCard}>
+      <Text style={styles.eventTitle}>{event.title}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  eventCard: {
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 12,
+    marginBottom: 10,
+    elevation: 2,
+  },
+  eventTitle: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
+});
