8a70c7cba746fbe8fdf23eb9abdc024960aae1b7
juan <juanjuangmo@gmail.com>
2025-11-08 17:41:43 +0100

    Defined event_datasource contract

--- domain/datasources/event_datasource.ts
+++ domain/datasources/event_datasource.ts
@@ -1,5 +1,30 @@
+import { EventParticipantResponseDTO } from "../model/dto/event_participant_response_dto";
+import { EventRequestDTO } from "../model/dto/event_request_dto";
+import { EventResponseDTO } from "../model/dto/event_response_dto";
+import { InterestTag } from "../model/enums/interest_tag";
 
 
+/**
+ * Remote DataSource contract for Events 
+ */
 export interface EventDataSource {
-    // Contract for event data source methods
+  getEventById(eventId: string): Promise<EventResponseDTO>;
+
+  getEventsByAnyTag(tags: InterestTag[]): Promise<EventResponseDTO[]>;
+
+  getEventsByDateAscending(eventDateISO: string): Promise<EventResponseDTO[]>;
+
+  getEventsByLocation(city: string): Promise<EventResponseDTO[]>;
+
+  getEventParticipants(eventId: string): Promise<EventParticipantResponseDTO[]>;
+
+  createEvent(request: EventRequestDTO): Promise<EventResponseDTO>;
+
+  subscribeToEvent(eventId: string): Promise<EventParticipantResponseDTO>;
+
+  updateEvent(eventId: string, request: EventRequestDTO): Promise<EventResponseDTO>;
+
+  deleteEvent(eventId: string): Promise<void>;
+
+  cancelEventSubscription(eventId: string): Promise<void>;
 }
